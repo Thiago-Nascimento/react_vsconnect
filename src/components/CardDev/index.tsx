90e73a2b2dbafed83c409c25c536c182a510ef27
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2023-08-16 18:52:33 -0300

    feat: atualização aula do dia 14/08

--- src/components/CardDev/index.tsx
+++ src/components/CardDev/index.tsx
@@ -5,16 +5,18 @@ export default function CardDev(props: any) {
     return (
         <div className="dev">
             <div className="grupo_contato">
-                <img src="https://github.com/Thiago-Nascimento.png" alt="" />
+                <img src={props.foto} alt="" />
                 <div className="contato_dev">
                     <h3>{ props.nome }</h3>
                     <p>{ props.email }</p>
                 </div>
             </div>
             <div className="techs">
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>React</span>
+            {
+                props.techs.map((tech: string, index: number) => {
+                    return <span key={index}>{tech}</span>
+                })
+            }
             </div>
         </div>
     )
