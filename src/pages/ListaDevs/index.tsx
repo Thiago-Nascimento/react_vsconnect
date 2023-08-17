6bb089c2d7f9f21c6e85f64d51dfe59110c3cc0b
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2023-08-17 19:07:58 -0300

    feat: Interação com APIs

--- src/pages/ListaDevs/index.tsx
+++ src/pages/ListaDevs/index.tsx
@@ -1,46 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import CardDev from "../../components/CardDev"
 import "./style.css"
 
+import api from "../../utils/api";
+
 export default function ListaDevs() {
 
-    const [devs, setDevs] = useState<any[]>([
-
-        {
-            img_perfil: "https://github.com/Thiago-Nascimento.png",
-            nome: "Thiago Nascimento",
-            email: "thiago@email.com",
-            skills: ["HTML", "CSS", "REACT"]
-        },
-        {
-            img_perfil: "https://github.com/JessicaSanto.png",
-            nome: "Jessica Franzon",
-            email: "jessica@email.com",
-            skills: ["HTML", "CSS", "REACT"]
-        },
-        {
-            img_perfil: "https://github.com/odirlei-assis.png",
-            nome: "Odirlei Sabella",
-            email: "odirlei@email.com",
-            skills: ["HTML", "CSS", "ANGULAR"]
-        },
-        {
-            img_perfil: "https://github.com/alexiamelhado18.png",
-            nome: "Aléxia Vitória",
-            email: "alexia@email.com",
-            skills: ["PYTHON", "VUE", "REACT"]
-        }
-
-    ]);
+    const [devs, setDevs] = useState<any[]>([]);
 
     const [skillDigitada, setSkillDigitada] = useState<string>("");
 
     const [listaDevsFiltrados, setListaDevsFiltrados] = useState<any[]>(devs);
 
+    useEffect( () => {
+        document.title = "Lista de Devs - VSConnect"
+
+        listarDesenvolvedores()
+    }, [] )
+    
     function buscarPorSkill(event: any){
         event.preventDefault();
 
-        const devsFiltrados = devs.filter((dev: any) => dev.skills.includes(skillDigitada.toLocaleUpperCase()));
+        const devsFiltrados = devs.filter((dev: any) => dev.hardSkills.includes(skillDigitada.toLocaleUpperCase()));
 
         if(devsFiltrados.length === 0){
             alert("Nenhum desenvolvedor(a) com essa skill")
@@ -51,9 +32,18 @@ export default function ListaDevs() {
 
     function retornoDevsGeral(event: any){
         if(event.target.value === ""){
-            setListaDevsFiltrados(devs)
+            listarDesenvolvedores()
         }
         setSkillDigitada(event.target.value)
+    }
+
+    function listarDesenvolvedores() {
+
+        api.get("users").then( (response: any) => {
+            console.log(response.data)
+            setDevs(response.data)
+        } )
+
     }
 
 
@@ -76,13 +66,13 @@ export default function ListaDevs() {
                         </form>
                         <div className="wrapper_lista">
                             <ul>
-                                {listaDevsFiltrados.map((dev: any, index: number) => {
-                                    return <li>
+                                {devs.map((dev: any, index: number) => {
+                                    return <li key={index}>
                                           <CardDev 
-                                          foto={dev.img_perfil}
+                                          foto={dev.user_img}
                                           nome={dev.nome}
                                           email={dev.email}
-                                          techs={dev.skills}
+                                          techs={dev.hardSkills}
                                            />
                                     </li>
                                 }
